c8ad908ab655ee0cd527f3f64a394cbfd415599a
ricard.vinas.ferran <ricard.vinasf@gmail.com>
2024-03-10 16:53:06 +0100

    creating users in database

--- culturapp/lib/main.dart
+++ culturapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:culturapp/routes/app_routes.dart';
 import 'package:culturapp/routes/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
--- culturapp/lib/pages/login.dart
+++ culturapp/lib/pages/login.dart
@@ -1,5 +1,7 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import "package:sign_in_button/sign_in_button.dart";
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,6 +14,8 @@ class _Login extends State<Login> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   User? _user;
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   @override
   void initState() {
@@ -36,11 +40,12 @@ class _Login extends State<Login> {
   Widget _googleSignInButton() {
     return Center(child: SizedBox(
       height: 50,
-      child: ElevatedButton(
+      child: SignInButton(
+        Buttons.google,
         onPressed: () {
           _handleGoogleSignIn();
         },
-        child: Text("Sign in with Google"),
+        text: "Sign in with Google"
       )
     ));
   }
@@ -70,14 +75,36 @@ class _Login extends State<Login> {
     );
   }
 
-  void _handleGoogleSignIn() {
+  Future<void> _handleGoogleSignIn() async {
     try {
       GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
-      _auth.signInWithProvider(_googleAuthProvider);
+      final UserCredential userCredential = await _auth.signInWithProvider(_googleAuthProvider);
+      bool userExists = await accountExists(userCredential.user);
+      if (userExists) {
+        print("User exists");
+      }
+      else {
+        print("User doens't exist");
+        createUser(_user);
+      }
     }
     catch (error) {
       print(error);
     }
+  }
+  
+  Future<bool> accountExists(User? user) async {
+    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection("users").doc(_user!.uid).get();
+    return userSnapshot.exists;
+  }
+  
+  void createUser(User? user) {
+    CollectionReference usersCollection = FirebaseFirestore.instance.collection('users');
+    usersCollection.doc(_user?.uid).set({
+      'email': _user?.email,
+      'name': _user?.displayName,
+    });
+    print("Account created");
   }
 }
 
