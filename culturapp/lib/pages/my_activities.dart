885aefb5767b639851a160996d4d259c52813d51
susssss23 <susannajaner@gmail.com>
2024-03-10 11:29:02 +0100

    page myactivities basico

---
+++ culturapp/lib/pages/my_activities.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class MyActivities extends StatefulWidget {
+  const MyActivities({super.key});
+
+  @override
+  State<MyActivities> createState() => _MyActivities();
+}
+
+class _MyActivities extends State<MyActivities> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
