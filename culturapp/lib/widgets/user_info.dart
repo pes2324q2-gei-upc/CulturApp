f37e9e2d68135c67f2cc13be493654409ec3ddce
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2024-03-11 13:02:52 +0100

    Update del perfil

--- culturapp/lib/widgets/user_info.dart
+++ culturapp/lib/widgets/user_info.dart
@@ -8,89 +8,128 @@ class UserInfoWidget extends StatefulWidget {
 
 class _UserInfoWidgetState extends State<UserInfoWidget> {
   String _selectedText = 'Historico actividades';
+  int _selectedIndex = 0;
+
+  void _onTabChange(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _selectedText = _getLabelText(index);
+    });
+  }
+
+  //texto que aparece al apretar uno de los botones
+  String _getLabelText(int index) {
+    switch (index) {
+      case 0:
+        return 'Historico actividades';
+      case 1:
+        return 'Insignias o logros';
+      default:
+        return '';
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     //faltaria adaptar el padding en % per a cualsevol dispositiu
+    //columna para la parte del username, xp i imagen perfil
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 55, bottom:20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/userImage.png',
-                  width: 100, // Adjust the width as needed
-                  height: 100, // Adjust the height as needed
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16, top: 55, bottom:20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //imagen
+            children: [
+              Image.asset(
+                'assets/userImage.png',
+                width: 100, 
+                height: 100, 
+              ),
+              const SizedBox(width: 10),
+              const Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //username
+                    Text(
+                      'Username',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 5),
+                    //XP
+                    Text(
+                      'XP',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-                const Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Username',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'XP',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(), 
-                IconButton(
-                  onPressed: () {
-                    //hacer que me lleve a una nueva pestaña con las opciones de configuración
-                  },
-                  icon: const Icon(Icons.settings),
-                ),
-              ],
-            ),
+              ),
+              //boton de settings
+              const Spacer(), 
+              IconButton(
+                onPressed: () {
+                  //hacer que me lleve a una nueva pestaña con las opciones de configuración
+                },
+                icon: const Icon(Icons.settings),
+              ),
+            ],
           ),
-          Container(
-            height: 50,
-            color: Colors.orange, 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //historico actividades
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _selectedText = 'Historico actividades';
-                    });
-                  },
-                  icon: const Icon(Icons.event_available),
-                  color: Colors.white,
-                ),
-                //insignias o logros
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _selectedText = 'Lista insignias';
-                    });
-                  },
-                  //icon: const Icon(Icons.redeem),
-                  icon: const Icon(Icons.stars),
-                  color: Colors.white,
-                ),
-              ],
-            ),
+        ),
+        //contenedor para elegir ver el historico o las insignias
+        Container(
+          height: 50,
+          color: Colors.orange,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(Icons.event_available, 'Historico actividades', 0),
+              _buildNavItem(Icons.stars, 'Insignias', 1),
+            ],
           ),
-          const SizedBox(height: 10),
-          Center(
-            child: Text(
-              _selectedText, // Display the selected text
-              style: const TextStyle(fontSize: 16, color: Colors.black),
-            ),
+        ),
+        //Texto de historico o insignias
+        const SizedBox(height: 10),
+        Center(
+          child: Text(
+            _selectedText, 
+            style: const TextStyle(fontSize: 16, color: Colors.black),
           ),
-        ],
-      );
+        ),
+      ],
+    );
+  }
+  
+  //NavItem de historico o insignias
+  Widget _buildNavItem(IconData icon, String label, int index) {
+    bool isSelected = index == _selectedIndex;
+    return GestureDetector(
+      onTap: () {
+        _onTabChange(index);
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.white : Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: isSelected ? Colors.orange : Colors.white),
+            const SizedBox(width: 8),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? Colors.orange : Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
