e13faef10cc2227b12f1390e7bd885aa6fd4b4ab
josep.valero.casas-aljama <josep.valero.casas-aljama@estudiantat.upc.edu>
2024-03-11 17:43:36 +0100

    dev -> database

--- culturapp/lib/routes/app_routes.dart
+++ culturapp/lib/routes/app_routes.dart
@@ -1,12 +1,9 @@
 import 'package:culturapp/map/map_screen.dart';
+import 'package:culturapp/pages/my_activities.dart';
 import 'package:culturapp/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:culturapp/perfil/perfil_screen.dart';
-
 
 Map<String, Widget Function(BuildContext)> appRoutes = {
   Routes.map: (_) => const MapPage(),
-  Routes.perfil: (_) => const PerfilPage()
+  Routes.misActividades: (_) => const MyActivities(),
 };
-
-
