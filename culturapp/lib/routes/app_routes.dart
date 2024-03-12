9340137c8312ea92e54acc1b05351afd74eabaf9
hossam.zerouali <hossam.zerouali@estudiantat.upc.edu>
2024-03-12 13:05:41 +0100

    semi merge

--- culturapp/lib/routes/app_routes.dart
+++ culturapp/lib/routes/app_routes.dart
@@ -1,12 +1,13 @@
 import 'package:culturapp/actividades/lista_actividades.dart';
 import 'package:culturapp/categorias_favoritas.dart/categorias.dart';
+import 'package:culturapp/controlador_presentacion.dart';
 import 'package:culturapp/map/map_screen.dart';
 import 'package:culturapp/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 
 Map<String, Widget Function(BuildContext)> appRoutes = {
-  Routes.map: (_) => const MapPage(),
   Routes.actividades: (_) => const ListaActividades(),
   Routes.categorias: (_) => const Categorias()
+  
 };
