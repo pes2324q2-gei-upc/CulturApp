c391dfa67c297737f65c542d4cec4aa7fe02138b
AleixFIB <aleix.lopez.padro@estudiantat.upc.edu>
2024-03-21 22:28:02 +0100

    revert with main index = 0

--- culturapp/lib/main.dart
+++ culturapp/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
   void _onTabChange(int index) {
     setState(() {
