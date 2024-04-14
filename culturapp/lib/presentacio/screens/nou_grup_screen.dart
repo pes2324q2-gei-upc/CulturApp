4f5b6f758b64d759faa1245b1ed2b028216329ac
susssss23 <susannajaner@gmail.com>
2024-04-14 10:17:27 +0200

    avançat pantalla crear grup, ara mateix no funciona

--- culturapp/lib/presentacio/screens/nou_grup_screen.dart
+++ culturapp/lib/presentacio/screens/nou_grup_screen.dart
@@ -1,7 +1,5 @@
 import "package:culturapp/presentacio/controlador_presentacio.dart";
-import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 
 class NouGrupScreen extends StatefulWidget {
   final ControladorPresentacion controladorPresentacion;
@@ -21,11 +19,30 @@ class _NouGrupScreen extends State<NouGrupScreen> {
     _controladorPresentacion = controladorPresentacion;
   }
 
+  static List<String> mockParticipants = [
+    'user1',
+    'user2',
+  ];
+
+  /*
+  si es fes un model per els participants dels grups hauria de tindre els atributs següents:
+  -nom
+  -foto
+  (aquests dos es podrien agafar dels usuaris)
+  - bool de si han sigut afegits
+  */
+
+  List<String> participantsAfegits = [];
+
   void updateList(String value) {
     //funcio on es filtrarà la nostra llista
     setState(
       () {},
     );
+  }
+
+  void afegirParticipant(participant) {
+    participantsAfegits.add(participant);
   }
 
   @override
@@ -50,22 +67,50 @@ class _NouGrupScreen extends State<NouGrupScreen> {
               ),
               const SizedBox(
                 child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      'Escolleix els participants del nou grup: ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blueGrey,
-                      ),
-                    )),
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'Escolleix els participants del nou grup: ',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ),
               ),
               _buildCercador(),
               const SizedBox(
                 height: 10.0,
               ),
-              const SizedBox(
-                child: Text('aqui anirien el llistat usuaris'),
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Afegits: ',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: ListView.builder(
+                      itemCount: participantsAfegits.length,
+                      itemBuilder: (context, index) =>
+                          _buildParticipantAfegit(context, index),
+                    ),
+                  ),
+                  //un altre element que seria el dels noms dels participants una vegada ficats dins
+                ],
+              ),
+              SizedBox(
+                height: 420.0,
+                child: ListView.builder(
+                  itemCount: mockParticipants.length,
+                  itemBuilder: (context, index) =>
+                      _buildParticipant(context, index),
+                ),
               ),
             ],
           ),
@@ -105,6 +150,48 @@ class _NouGrupScreen extends State<NouGrupScreen> {
             suffixIconColor: Colors.white,
           ),
         ));
+  }
+
+  Widget _buildParticipantAfegit(context, index) {
+    return ListTile(
+      title: Text(
+        participantsAfegits[index],
+      ),
+    );
+  }
+
+  Widget _buildParticipant(context, index) {
+    return ListTile(
+      //una vegada tingui mes info del model
+      //dels perfils lo seu seria canviar-ho
+      contentPadding: EdgeInsets.all(8.0),
+      leading: Image.network(
+        'https://w7.pngwing.com/pngs/635/97/png-transparent-computer-icons-the-broadleaf-group-people-icon-miscellaneous-monochrome-black.png',
+        fit: BoxFit.cover,
+        width: 50,
+        height: 50,
+      ),
+      title: Text(mockParticipants[index],
+          style: const TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+          )),
+      trailing: _buildBotoAfegir(mockParticipants[index]),
+    );
+  }
+
+  Widget _buildBotoAfegir(participant) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(),
+        backgroundColor: const Color.fromRGBO(240, 186, 132, 1),
+        foregroundColor: Colors.white,
+      ),
+      onPressed: () {
+        afegirParticipant(participant);
+      },
+      child: Text('+'),
+    );
   }
 
   Widget _buildNextPageButton() {
