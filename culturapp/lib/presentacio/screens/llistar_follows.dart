01044710857ed4d5521aa01270e57417df07949e
pablo.aparici <pablo.aparici@estudiantat.upc.edu>
2024-04-30 16:58:21 +0200

    Afegir slider corregit

--- culturapp/lib/presentacio/screens/llistar_follows.dart
+++ culturapp/lib/presentacio/screens/llistar_follows.dart
@@ -10,9 +10,10 @@ class LlistarFollows extends StatefulWidget {
   _LlistarFollowsState createState() => _LlistarFollowsState();
 }
 
-class _LlistarFollowsState extends State<LlistarFollows> {
+class _LlistarFollowsState extends State<LlistarFollows> with SingleTickerProviderStateMixin {
   late List<String> users;
   late bool isFollows;
+  late TabController _tabController;
 
   _LlistarFollowsState() {
     users = [];
@@ -47,20 +48,20 @@ class _LlistarFollowsState extends State<LlistarFollows> {
     return getUsers(token, 'following');
   }
 
-void updateUsers() async {
-  List<String> updatedUsers;
-  if (isFollows) {
-    updatedUsers = await getFollowersUser(token);
-  } else {
-    updatedUsers = await getFollowingsUser(token);
+  void updateUsers() async {
+    List<String> updatedUsers;
+    if (isFollows) {
+      updatedUsers = await getFollowersUser(token);
+    } else {
+      updatedUsers = await getFollowingsUser(token);
+    }
+    setState(() {
+      users = updatedUsers;
+    });
   }
-  setState(() {
-    users = updatedUsers;
-  });
-}
 
   List<String> originalUsers = [];
-  
+
   void updateList(String value) {
     setState(() {
       if (originalUsers.isEmpty) {
@@ -69,19 +70,34 @@ void updateUsers() async {
       users = originalUsers.where((element) => element.toLowerCase().contains(value.toLowerCase())).toList();
     });
   }
+
   @override
   void initState() {
     super.initState();
     isFollows = true;
     updateUsers();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        isFollows = _tabController.index == 0;
+        updateUsers();
+      });
+    });
   }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFFF4692A),
         title: const Text("Amics"),
-        centerTitle: true, // Centrar el título
+        centerTitle: true,
         toolbarHeight: 50.0,
         titleTextStyle: const TextStyle(
           color: Colors.white,
@@ -90,136 +106,58 @@ void updateUsers() async {
         ),
       ),
       body: Column(
-  children: [
-    SizedBox(height: 16.0), // Add spacing of 8.0
-    Expanded(
-      child: DefaultTabController(
-        length: 2,
-        child: Column(
-          children: [
-            const TabBar(
-              tabs: [
-                Tab(text: "Follows"),
-                Tab(text: "Followings"),
+        children: [
+          SizedBox(height: 16.0),
+          TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(text: "Followers"),
+              Tab(text: "Followings"),
+            ],
+            indicatorColor: Colors.orange,
+            labelColor: Colors.orange,
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                _buildFollowersView(),
+                _buildFollowingsView(),
               ],
-              indicatorColor: Colors.orange,
-              labelColor: Colors.orange,
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  Column(
-                    children: [
-                      const SizedBox(height: 10.0),
-                      SizedBox(
-                        height: 45.0,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-                          child: TextField(
-                            onChanged: (value) {
-                              updateList(value);
-                            },
-                            cursorColor: Colors.white,
-                            cursorHeight: 20,
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: const Color(0xFFFFAA80),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8.0),
-                                borderSide: BorderSide.none,
-                              ),
-                              hintText: "Search...",
-                              hintStyle: const TextStyle(
-                                color: Colors.white,
-                              ),
-                              suffixIcon: const Icon(Icons.search),
-                              suffixIconColor: Colors.white,
-                              contentPadding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5.0), // Add padding top of 5.0
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: users.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                userBox(text: users[index], recomm: false),
-                                const SizedBox(height: 5.0), // Add padding top of 5.0
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  // Repeat the same structure for the second tab
-                  Column(
-                    children: [
-                      const SizedBox(height: 10.0),
-                      SizedBox(
-                        height: 45.0,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-                          child: TextField(
-                            onChanged: (value) {
-                              updateList(value);
-                            },
-                            cursorColor: Colors.white,
-                            cursorHeight: 20,
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: const Color(0xFFFFAA80),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8.0),
-                                borderSide: BorderSide.none,
-                              ),
-                              hintText: "Search...",
-                              hintStyle: const TextStyle(
-                                color: Colors.white,
-                              ),
-                              suffixIcon: const Icon(Icons.search),
-                              suffixIconColor: Colors.white,
-                              contentPadding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5.0), // Add padding top of 5.0
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: users.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                userBox(text: users[index], recomm: false),
-                                const SizedBox(height: 5.0), // Add padding top of 5.0
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-    ),
-  ],
-),
+    );
+  }
 
- }
+  Widget _buildFollowersView() {
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            userBox(text: users[index], recomm: false),
+            const SizedBox(height: 5.0),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildFollowingsView() {
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            userBox(text: users[index], recomm: false),
+            const SizedBox(height: 5.0),
+          ],
+        );
+      },
+    );
+  }
 }
 
 void main() {
