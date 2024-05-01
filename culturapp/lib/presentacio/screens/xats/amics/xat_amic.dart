0254f7773957897eecde06cf8f1a6297895390b0
susssss23 <susannajaner@gmail.com>
2024-05-01 17:10:39 +0200

    els missatges fan el scroll adecuat i es fa print del missatge quan s'envia

--- culturapp/lib/presentacio/screens/xats/amics/xat_amic.dart
+++ culturapp/lib/presentacio/screens/xats/amics/xat_amic.dart
@@ -25,17 +25,21 @@ class _XatAmicScreen extends State<XatAmicScreen> {
   late Usuari _usuari; //usuari utilitzant l'aplicació
   late xatAmic xat;
   late List<Message> messages;
+  late ScrollController _scrollController;
 
   Color taronjaFluix = const Color.fromRGBO(240, 186, 132, 1);
   Color grisFluix = const Color.fromRGBO(211, 211, 211, 0.5);
+
+  final TextEditingController _controller = TextEditingController();
 
   _XatAmicScreen(
       ControladorPresentacion controladorPresentacion, Usuari usuari) {
     _controladorPresentacion = controladorPresentacion;
     _usuari = usuari;
+    messages = [];
+    _scrollController = ScrollController();
+    _loadMessages();
   }
-
-  final TextEditingController _controller = TextEditingController();
 
   void _sendMessage(String text) {
     //enviar missatge, jo envio missatge
@@ -48,6 +52,8 @@ class _XatAmicScreen extends State<XatAmicScreen> {
         String time = Timestamp.now().toDate().toIso8601String();
         _controladorPresentacion.addXatMessage(_usuari.id, time, text);
 
+        time = convertTimeFormat(time);
+
         messages.insert(
           messages.length,
           Message(text: text, sender: 'Me', timeSended: time),
@@ -56,32 +62,14 @@ class _XatAmicScreen extends State<XatAmicScreen> {
     }
   }
 
-  void assignaUsernames(Message message, void Function(String) callback) {
-    _controladorPresentacion.getUsername(message.sender).then((username) {
-      if (username == null) {
-        // Handle the case where username is null
-        callback('Default');
-      } else {
-        callback(username);
-      }
-    });
-  }
-
   Future<List<Message>> getMessage() async {
     List<Message> msg =
         await _controladorPresentacion.getXatMessages(_usuari.id);
     return msg;
   }
 
-  Future<String> getUserNameById(String id) {
-    return _controladorPresentacion.getUsername(id);
-  }
-
   Message convertIntoRigthVersion(Message message) {
     //una funcio que converteix el temps en apropiat i el id del ususari en el nom de l'usuari
-    assignaUsernames(message, (username) {
-      message.sender = username;
-    });
 
     message.timeSended = convertTimeFormat(message.timeSended);
     return message;
@@ -91,57 +79,53 @@ class _XatAmicScreen extends State<XatAmicScreen> {
     return messagelist.map(convertIntoRigthVersion).toList();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<Message>>(
-      future: getMessage(),
-      builder: (BuildContext context, AsyncSnapshot<List<Message>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return _buildLoadingScreen();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}'); // Si hi ha hagut algun error
-        } else {
-          //convertim els missatges en la versió correcta quan els agafem del backend i ens ho guardem al frontend
-          messages = convertData(snapshot.data!);
-          return Scaffold(
-            resizeToAvoidBottomInset: true,
-            appBar: _buildAppBar(),
-            body: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: grisFluix,
-                    alignment: Alignment.topCenter,
-                    child: ListView.builder(
-                      reverse: true,
-                      shrinkWrap: true,
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        return _buildChatBubble(messages[index]);
-                      },
-                    ),
-                  ),
-                ),
-                const Divider(height: 1.0),
-                Container(
-                  decoration: BoxDecoration(color: Theme.of(context).cardColor),
-                  child: _bottomInputField(),
-                ),
-              ],
-            ),
-          );
-        }
-      },
+  Future<void> _loadMessages() async {
+    List<Message> loadedMessages =
+        await _controladorPresentacion.getXatMessages(_usuari.id);
+    setState(() {
+      messages = convertData(loadedMessages);
+    });
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
     );
   }
 
-  Widget _buildLoadingScreen() {
-    return const SizedBox(
-      width: 5,
-      height: 5,
-      child: CircularProgressIndicator(
-        strokeWidth: 1,
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: _buildAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: grisFluix,
+              alignment: Alignment.topCenter,
+              child: ListView.builder(
+                controller: _scrollController,
+                shrinkWrap: true,
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return _buildChatBubble(messages[index]);
+                },
+              ),
+            ),
+          ),
+          const Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _bottomInputField(),
+          ),
+        ],
       ),
     );
   }
