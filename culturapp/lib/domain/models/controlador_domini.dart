87259e213b9efe477d95a06f109fe07ea96d0a57
Rosa <rosa.hortelano@estudiantat.upc.edu>
2024-05-09 14:00:00 +0200

    modificar controlador a localhost

--- culturapp/lib/domain/models/controlador_domini.dart
+++ culturapp/lib/domain/models/controlador_domini.dart
@@ -824,7 +824,7 @@ class ControladorDomini {
     try {
       final respuesta = await http.get(
         Uri.parse(
-            'https://culturapp-back.onrender.com/xats/exists?receiverId=$receiverName'),
+            'http://{$ip}:8080/xats/exists?receiverId=$receiverName'),
         headers: {
           'Authorization': 'Bearer ${userLogged.getToken()}',
         },
