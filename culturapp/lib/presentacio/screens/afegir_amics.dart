c9ba69c8d02ac37e0c04015f633fb48406ab4799
susssss23 <susannajaner@gmail.com>
2024-05-09 12:07:39 +0200

    afegir amics cercador

--- culturapp/lib/presentacio/screens/afegir_amics.dart
+++ culturapp/lib/presentacio/screens/afegir_amics.dart
@@ -6,109 +6,116 @@ import "package:flutter/material.dart";
 
 class AfegirAmics extends StatefulWidget {
   @override
-  final List<Usuario>recomms;
-  final List<Usuario>usersBD;
+  final List<Usuario> recomms;
+  final List<Usuario> usersBD;
   final ControladorPresentacion controladorPresentacion;
-  const AfegirAmics({super.key, required this.recomms, required this.controladorPresentacion, required this.usersBD});
-  
+  const AfegirAmics(
+      {super.key,
+      required this.recomms,
+      required this.controladorPresentacion,
+      required this.usersBD});
+
   @override
-  State<StatefulWidget> createState()  => _AfegirAmicsState(recomms, usersBD);
+  State<StatefulWidget> createState() => _AfegirAmicsState(recomms, usersBD);
 }
 
 class _AfegirAmicsState extends State<AfegirAmics> {
-  late List<Usuario>usersRecom;
-  late List<Usuario>usersBD;
-  _AfegirAmicsState(List<Usuario>recomms, List<Usuario>usBD){
+  late List<Usuario> usersRecom;
+  late List<Usuario> usersBD;
+  _AfegirAmicsState(List<Usuario> recomms, List<Usuario> usBD) {
     usersRecom = recomms;
     usersBD = usBD;
   }
 
   String value = '';
 
-void updateList(String newValue) {
-  setState(() {
-    value = newValue;
-    usersBD = widget.usersBD
-        .where((user) => user.username.toLowerCase().contains(value.toLowerCase()))
-        .toList();
-  });
-}
+  Color taronjaVermellos = const Color(0xFFF4692A);
+  Color taronjaVermellosFluix = const Color.fromARGB(199, 250, 141, 90);
 
-@override
-Widget build(BuildContext context) {
-  return Column(
-    children: [
-      SizedBox(
-        height: 45.0,
-        child: Padding(
-          padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-          child: TextField(
-            onChanged: (value) {
-              updateList(value);
-            },
-            cursorColor: Colors.white,
-            cursorHeight: 20,
-            style: const TextStyle(
-              color: Colors.white,
-            ),
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: const Color.fromRGBO(240, 186, 132, 1),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide.none,
-              ),
-              hintText: "search".tr(context),
-              hintStyle: const TextStyle(
+  void updateList(String newValue) {
+    setState(() {
+      value = newValue;
+      usersBD = widget.usersBD
+          .where((user) =>
+              user.username.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 45.0,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+            child: TextField(
+              onChanged: (value) {
+                updateList(value);
+              },
+              cursorColor: Colors.white,
+              cursorHeight: 20,
+              style: const TextStyle(
                 color: Colors.white,
               ),
-              suffixIcon: const Icon(Icons.search),
-              suffixIconColor: Colors.white,
-              contentPadding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: taronjaVermellosFluix,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: "search".tr(context),
+                hintStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+                suffixIcon: const Icon(Icons.search),
+                suffixIconColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 12.0, horizontal: 12.0),
+              ),
             ),
           ),
         ),
-      ),
-      SizedBox(
-        child: Column(
-          children: [
-            
-            const Padding(padding: EdgeInsets.only(top: 20.0)),
-            if (value == "" || value == " ")
-              ...[
-              Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "recommendations".tr(context),
-                style: TextStyle(
-                  color: const Color(0xFFF4692A),
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
+        SizedBox(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 20.0)),
+              if (value == "" || value == " ") ...[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "recommendations".tr(context),
+                    style: TextStyle(
+                      color: taronjaVermellos,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(10.0)),
+                const Padding(padding: EdgeInsets.all(10.0)),
                 for (var user in usersRecom)
                   Padding(
                     padding: const EdgeInsets.only(top: 5.0),
                     child: userBox(
-                      text: user.username, 
-                      recomm: true, 
-                      type: "addSomeone", 
-                      controladorPresentacion: widget.controladorPresentacion),
+                        text: user.username,
+                        recomm: true,
+                        type: "addSomeone",
+                        controladorPresentacion:
+                            widget.controladorPresentacion),
                   ),
-              ]
-            else 
-            ...[
-              for (var user in usersBD)
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: userBox(
-                  text: user.username, 
-                  recomm: false,  
-                  type: "null", 
-                  controladorPresentacion: widget.controladorPresentacion),
-              ),
+              ] else ...[
+                for (var user in usersBD)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: userBox(
+                        text: user.username,
+                        recomm: false,
+                        type: "null",
+                        controladorPresentacion:
+                            widget.controladorPresentacion),
+                  ),
               ]
             ],
           ),
